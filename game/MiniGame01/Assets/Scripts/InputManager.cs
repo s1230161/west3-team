70db9bfcdb9c650d88783f0ce4f2a86e8348676d
= <s1230161@shin-PC.localdomain>
2018-04-23 18:08:18 +0900

    新規登録機能の実装

--- game/MiniGame01/Assets/Scripts/InputManager.cs
+++ game/MiniGame01/Assets/Scripts/InputManager.cs
@@ -6,6 +6,9 @@ using UnityEngine.Networking;
 public class InputManager : MonoBehaviour {
 
 	InputField inputField;
+	UnityWebRequest request;
+
+	public Text errorMessage;
 
 
 	/// <summary>
@@ -17,6 +20,7 @@ public class InputManager : MonoBehaviour {
 		inputField = GetComponent<InputField>();
 
 		InitInputField();
+
 
 	}
 
@@ -33,10 +37,14 @@ public class InputManager : MonoBehaviour {
 		string inputValue = inputField.text;
 
 		//Debug.Log(inputValue);
-		ConnectionStart (inputValue);
+		if (inputValue.Length >= 5) {
+			errorMessage.text = ("読み込み中");
+			ConnectionStart (inputValue);
+		}else
+			errorMessage.text = ("5文字以上で入力してください。");
 		InitInputField();
-		if(inputValue!="")
-		Application.LoadLevel("Main");
+		//if(inputValue!="")
+			//Application.LoadLevel("Main");
 	}
 
 
@@ -58,22 +66,35 @@ public class InputManager : MonoBehaviour {
 
 	public void ConnectionStart(string inputvalue){
 		StartCoroutine(Connection(inputvalue));
-		Debug.Log (inputvalue);
+		//Debug.Log (inputvalue);
 	}
 
 	private IEnumerator Connection(string name)
 	{
 		WWWForm form = new WWWForm();
 		form.AddField("name", name);
-		UnityWebRequest request = UnityWebRequest.Post("https://myapp.com", form);
+		request = UnityWebRequest.Post("http://192.168.11.21:3000/game/create", form);
+		request.downloadHandler = new DownloadHandlerBuffer();
+		//UnityWebRequest request = UnityWebRequest.Get("http://192.168.11.21:3000/game/please");
+		//request.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
+		//WWW www = new WWW("http://192.168.11.21:3000/game/create",form);
+		//yield return www;
+		//if(www.error == null) debug
+		//Debug.Log ("受け取った文字列： " + request.downloadHandler.text);
 
 		// リクエスト送信
-		yield return request.Send();
+		yield return request.SendWebRequest();
+		Debug.Log ("受け取った文字列： " + request.downloadHandler.text);
+		if (request.downloadHandler.text == "0")
+			errorMessage.text = (name + "はすでに使われています。");
+		else if (request.downloadHandler.text == "1")
+			Application.LoadLevel("Main");
+		
 
 		if (request.isNetworkError) {
 			Debug.Log ("エラー:" + request.error);
 		} else {
-			if (request.responseCode == 204) {
+			if (request.responseCode == 200) {
 				Debug.Log ("せいこう！");
 			} else {
 				Debug.Log ("しっぱい…:" + request.responseCode);
